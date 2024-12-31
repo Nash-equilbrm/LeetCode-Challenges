c50d72944652e64741db508f6cee97d240c2b65b
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2024-12-31 14:34:45 +0700

    Time: 1 ms (36.2%), Space: 12.7 MB (62.37%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -8,11 +8,10 @@ public:
                 travelCost[i] = travelCost[i-1];
             }
             else{
-                int case1 = travelCost[i-1] + costs[0];
-                int case7 = travelCost[i-7] + costs[1];
-                int case30 = travelCost[i-30] + costs[2];
-            
-                travelCost[i] = min({case1, case7, case30});
+                travelCost[i] = min({
+                                travelCost[i-1] + costs[0],
+                                travelCost[i-7] + costs[1],
+                                travelCost[i-30] + costs[2]});
             }
         }
     
@@ -21,10 +20,7 @@ public:
 
     bool vectorContains(vector<int>& vec, int target){
         auto it = find(vec.begin(), vec.end(), target);
-        // Check if the target value was found
-        if (it!=vec.end())
-            return true;
-        return false;
+        return it!=vec.end();
     }
 
     
