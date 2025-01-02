3da2076669aafbff7970261261cc836a8b2f7908
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-01-02 11:11:24 +0700

    Time: 0 ms (100%), Space: 9.4 MB (15.26%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int maxScore(string s) {
-      
+        vector<pair<int,int>> countDigitLeft (s.length() + 1, {0,0});
+        for(int i = 0; i < s.length(); ++i){
+            if(s[i] == '0') {
+                countDigitLeft[i + 1].first = countDigitLeft[i].first + 1;
+                countDigitLeft[i + 1].second = countDigitLeft[i].second;
+            }
+            if(s[i] == '1') {
+                countDigitLeft[i + 1].first = countDigitLeft[i].first;
+                countDigitLeft[i + 1].second = countDigitLeft[i].second + 1;
+            }
+        }
+        int L, R;
         int max = 0;
-        for(int i = 1; i < s.length(); ++i){
-            int l = 0;
-            int r = 0;
-            string left = s.substr(0, i);
-            string right = s.substr(i, s.length() - i);
-            for(char& c : left) {
-                if(c == '0') l++;
-            }
-            for(char& c : right) {
-                if(c == '1') r++;
-            }
-            if(l + r >= max) max = l + r;
+        for(int i = 1; i < countDigitLeft.size() - 1; ++i){
+            L = countDigitLeft[i].first;
+            R = countDigitLeft.back().second - countDigitLeft[i].second;
+            if(L + R > max) max = L + R;
         }
         return max;
     }
