56fd29294e80195c2a89908fd6b8b8ea33d3f431
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-01-03 09:00:09 +0700

    Time: 9 ms (14.63%), Space: 106.1 MB (6.2%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -3,13 +3,13 @@ public:
     int waysToSplitArray(vector<int>& nums) {
         int cnt = 0;
         long long int sum = 0;
-        long long int leftSum = 0;
+        vector<long long int> leftSum;
         for (auto & i : nums) {
             sum += i;
+            leftSum.emplace_back(sum);
         }
         for(int i = 0; i < nums.size() - 1; ++i){
-            leftSum += nums[i];
-            if(leftSum >= sum - leftSum) cnt++;
+            if(leftSum[i] >= sum - leftSum[i]) cnt++;
         }
         return cnt;
     }
