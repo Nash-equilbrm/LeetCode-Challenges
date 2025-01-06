b93f4404578c29fe1039123008d1ee88efbd3a1e
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-01-07 01:28:36 +0700

    Time: 105 ms (78.28%), Space: 111.5 MB (7.06%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
+        map<char, vector<int>> cache;
         int count = 0;
         string alphabet = "abcdefghijklmnopqrstuvwxyz";
-        for(char& outside:alphabet){
-            vector<int> o = findInString(s, outside);
-            if(o.size() <= 0) continue;
+        for (char& outside : alphabet) {
+            vector<int> o;
+            if (cache.find(outside) != cache.end()) {
+                o = cache[outside];
+            } else {
+                o = findInString(s, outside);
+                cache.insert({outside, o});
+            }
+            if (o.size() <= 0)
+                continue;
             count += (o.size() >= 3) ? 1 : 0;
-            for(char& inside:alphabet){
-                if(inside == outside) continue;
-                vector<int> i = findInString(s, inside);
-                if(i.size() <= 0) continue;
-                for(int& i_index:i){
-                    if(i_index > o.front() && i_index < o.back()) {
+            for (char& inside : alphabet) {
+                if (inside == outside)
+                    continue;
+                vector<int> i;
+                if (cache.find(inside) != cache.end()) {
+                    i = cache[inside];
+                } else {
+                    i = findInString(s, inside);
+                    cache.insert({inside, i});
+                }
+                if (i.size() <= 0)
+                    continue;
+                for (int& i_index : i) {
+                    if (i_index > o.front() && i_index < o.back()) {
                         count++;
                         break;
                     }
@@ -22,10 +38,11 @@ public:
         return count;
     }
 
-    vector<int> findInString(string& s, char& c){
+    vector<int> findInString(string& s, char& c) {
         vector<int> res;
-        for(int i = 0; i < s.length(); ++i){
-            if(s[i] == c) res.emplace_back(i);
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == c)
+                res.emplace_back(i);
         }
         return res;
     }
