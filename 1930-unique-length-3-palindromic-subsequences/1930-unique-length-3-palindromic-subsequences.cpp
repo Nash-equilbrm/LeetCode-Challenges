bdb238373e6af69c08b5e330e97f082b300ef0a7
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-01-07 01:34:50 +0700

    Time: 98 ms (78.54%), Space: 112.5 MB (7.04%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -5,26 +5,14 @@ public:
         int count = 0;
         string alphabet = "abcdefghijklmnopqrstuvwxyz";
         for (char& outside : alphabet) {
-            vector<int> o;
-            if (cache.find(outside) != cache.end()) {
-                o = cache[outside];
-            } else {
-                o = findInString(s, outside);
-                cache.insert({outside, o});
-            }
+            vector<int> o = findInString(s, outside, cache);
             if (o.size() <= 0)
                 continue;
             count += (o.size() >= 3) ? 1 : 0;
             for (char& inside : alphabet) {
                 if (inside == outside)
                     continue;
-                vector<int> i;
-                if (cache.find(inside) != cache.end()) {
-                    i = cache[inside];
-                } else {
-                    i = findInString(s, inside);
-                    cache.insert({inside, i});
-                }
+                vector<int> i = findInString(s, inside, cache);
                 if (i.size() <= 0)
                     continue;
                 for (int& i_index : i) {
@@ -38,12 +26,16 @@ public:
         return count;
     }
 
-    vector<int> findInString(string& s, char& c) {
+    vector<int> findInString(string& s, char& c, map<char, vector<int>>& cache) {
+        if (cache.find(c) != cache.end()) {
+            return cache[c];
+        }
         vector<int> res;
         for (int i = 0; i < s.length(); ++i) {
             if (s[i] == c)
                 res.emplace_back(i);
         }
+        cache.insert({c, res});
         return res;
     }
 };
