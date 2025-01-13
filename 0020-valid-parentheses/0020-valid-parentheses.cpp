f1b76d11a1fba32d3d3ff9b4601573e6b10f03da
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-01-13 23:49:55 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length() % 2 == 1) return false;
+        stack<char> st;
+        int i = 0;
+        while(i < s.length()) {
+            char c = s[i];
+            switch(c) {
+                case '(': case '[': case '{':
+                    st.push(c);
+                    break;
+                case ')':
+                    if(st.top() == '(') {
+                        st.pop();
+                        break;
+                    }
+                    return false;
+                case ']':
+                    if(st.top() == '[') {
+                        st.pop();
+                        break;
+                    }
+                    return false;
+                case '}':
+                    if(st.top() == '{') {
+                        st.pop();
+                        break;
+                    }
+                    return false;
+            }
+            ++i;
+        }
+        return st.size() == 0;
+    }
+};
