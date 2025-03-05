b5342ecd42345240ea7e67bb66598dc00769335e
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-05 10:41:46 +0700

    Time: 3 ms (84.79%), Space: 19.6 MB (29.77%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,11 +3,9 @@ public:
     const int MAX_COUNT = 2;
     int removeDuplicates(vector<int>& nums) {
         int j = 0;
-        int cnt = 0;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (j == 0 || j == 1 || nums[i] != nums[j - 2]) {
-                nums[j++] = nums[i];
+        for (int num : nums) {
+            if (j < 2 || num != nums[j - 2]) {
+                nums[j++] = num;
             }
         }
         return j;
