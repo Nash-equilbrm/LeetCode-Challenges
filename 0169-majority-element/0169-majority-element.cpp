0de2440ecd808133f2238f6811985ff03ab5d4e5
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-05 11:00:07 +0700

    Time: 0 ms (100%), Space: 28.3 MB (24.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int vote = 0;
+        int cur = -1000000001;
+        for(int i = 0 ; i < nums.size(); ++i){
+            if(vote == 0){
+                cur = nums[i];
+                vote++;
+            }
+            else if(nums[i] == cur) vote++;
+            else if(nums[i] != cur) vote--;
+        }
+        return cur;
+    }
+};
