c80f41f91019a2f4de108066db12e5087e7470e1
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-05 11:27:51 +0700

    Time: 0 ms (100%), Space: 29.4 MB (97.27%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -10,7 +10,7 @@ public:
     
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k %= n;
+        k %= n;  // To avoid unnecessary full rotations
         reverse(nums, 0, n - 1);
         reverse(nums, 0, k - 1);
         reverse(nums, k, n - 1);
