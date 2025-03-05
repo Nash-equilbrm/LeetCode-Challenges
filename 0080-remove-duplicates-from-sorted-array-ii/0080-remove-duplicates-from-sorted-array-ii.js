954b8f9c27eeee00a812e39777c7b4eb4ac8b35d
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-05 10:46:03 +0700

    Time: 69 ms (29.09%), Space: 58.9 MB (9.97%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    if(nums.length <= 2) return nums.length;
     let j = 0;
     for(let i = 0 ; i < nums.length; ++i){
         if(j < 2 || nums[i] !== nums[j - 2]){
