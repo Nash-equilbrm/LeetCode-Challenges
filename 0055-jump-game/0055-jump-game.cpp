6da7d5787f5155fa2cfad419c48444955e6aa2d5
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-06 08:47:33 +0700

    Time: 0 ms (100%), Space: 52.1 MB (85.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size() == 1) return true;
-        if(nums[0] == 0) return false;
-        vector<bool> canArrive(nums.size(), false);
-        canArrive[nums.size() - 1] = true;
-        for(int i = nums.size() - 2; i >= 0; --i){
-            if(nums[i] == 0) continue;
-            else{
-                for(int step = 1; step <= nums[i]; ++ step){
-                    if(canArrive[i + step])
-                    {
-                        canArrive[i] = true;
-                        break;
-                    }
-                }
-            }
+        int maxReach = 0;
+        int size = nums.size();
+        for(int i = 0; i < size; ++i){
+            if(i > maxReach) return false;
+            maxReach = max(i + nums[i], maxReach);
+            if(maxReach >= size - 1) return true;
         }
-
-        return canArrive[0];
+        return false;
     }
 };
