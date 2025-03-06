3f6693549b3ad48041e0cf0cbc249a2274d9a201
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-06 15:10:41 +0700

    Time: 0 ms (100%), Space: 20.6 MB (34.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int size = nums.size();
-        vector<int> minStep(size, 0);
-        minStep[size - 1] = 0;
-        for(int i = size - 2; i >= 0; --i){
-            minStep[i] = 10001;
-            for(int step = 1; step <= nums[i]; ++step){
-                if(i + step >= size - 1){
-                    minStep[i] = 1;
-                    continue;
-                }
-                minStep[i] = min(minStep[i + step] + 1, minStep[i]);
+        if(size == 1) return 0;
+        int jump = 0, furthestReach = 0, curEnd = 0;
+        for(int i = 0; i < size; ++i){
+            furthestReach = max(furthestReach, i + nums[i]);
+            if(i == curEnd){
+                jump++;
+                curEnd = furthestReach;
+                if(furthestReach >= size - 1) break;
             }
         }
-        return minStep[0];
+        return jump;
     }
 };
