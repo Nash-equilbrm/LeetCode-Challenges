3fac4ce3f92059c17408fd5f2b0c9ce800d1e2c5
Hoài Nam <66255337+Nash-equilbrm@users.noreply.github.com>
2025-03-10 14:27:24 +0700

    Time: 0 ms (100%), Space: 9.2 MB (6.36%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -3,6 +3,6 @@ public:
     bool isPowerOfThree(int n) {
         if(n == 0) return false;
         double logResult = log10(n) / log10(3);
-        return (logResult - floor(logResult)) < 1e-10;;
+        return (logResult - floor(logResult)) < 1e-10;
     }
 };
